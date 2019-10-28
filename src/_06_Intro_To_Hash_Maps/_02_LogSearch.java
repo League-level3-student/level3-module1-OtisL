7146cdcec59aec6dcb7ac463d2407d4a226a2a20
OtisL <42128680+OtisL@users.noreply.github.com>
2019-10-28 16:55:50 -0700

    StringIntegerPair, IntroToHashMaps, LogSearch

--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,7 +1,15 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class _02_LogSearch {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+
+public class _02_LogSearch implements ActionListener {
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -29,5 +37,49 @@ public class _02_LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
-	
+	HashMap<Integer, String> hash = new HashMap<Integer, String>();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton();
+	JButton search = new JButton();
+	JButton list = new JButton();
+	JButton remove = new JButton();
+	public static void main(String[] args) {
+		_02_LogSearch yeeted = new _02_LogSearch();
+		yeeted.doStuff();
+	}
+	void doStuff() {
+		frame.add(panel);
+		panel.add(add);
+		panel.add(search);
+		panel.add(list);
+		add.addActionListener(this);
+		search.addActionListener(this);
+		list.addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(add)) {
+			String id = JOptionPane.showInputDialog("What ID should this be (a number)");
+			int ID = Integer.parseInt(id);
+			String name = JOptionPane.showInputDialog("What name would you like this to be?");
+			hash.put(ID, name);
+		}else if(e.getSource().equals(search)) {
+			String wantedID = JOptionPane.showInputDialog("What ID do you want to look up?");
+			int IDWanted = Integer.parseInt(wantedID);
+			boolean found=false;
+			for(int i=0; i<hash.size(); i++) {
+				if(hash.containsKey(i)) {
+					found=true;
+					JOptionPane.showMessageDialog(null, "The person at that key is "+hash.get(i));
+				}
+			}
+			if(!found) {
+				JOptionPane.showMessageDialog(null, "Oh no! That key doesn't exist!", "OOF", JOptionPane.ERROR_MESSAGE);
+			}
+		}else if(e.getSource().equals(list)) {
+			//NEED TO FINISH THE LIST THEN THE REMOVE!!!
+		}
+	}
 }
